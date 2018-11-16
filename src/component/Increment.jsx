fc83af6dd0f7784c0a2ea36a9b7d7ec6553792fa
hall liu <lyp1984831@hotmail.com>
2018-11-16 15:48:12 +0800

    first commit

---
+++ src/component/Increment.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Increment extends Component {
+    state = {  }
+    render() { 
+        return ( <div>Hello</div> );
+    }
+}
+ 
+export default Increment;
